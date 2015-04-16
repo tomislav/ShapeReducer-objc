5a19352578ae821a70fd5fce6016937a51b95928
Shri <shri.tekale@gmail.com>
2015-04-16 20:39:15 +0100

    Moved to ARC, removed forced sorting

--- ShapeReducer/ShapeReducer.h
+++ ShapeReducer/ShapeReducer.h
@@ -1,27 +1,21 @@
 #import <Foundation/Foundation.h>
 
 @interface ShapePoint : NSObject {
-    double latitude;
-    double longitude;
-    unsigned int sequence;
 }
 
-@property (nonatomic, assign) double latitude;
-@property (nonatomic, assign) double longitude;
-@property unsigned int sequence;
+@property (nonatomic, readonly) double latitude;
+@property (nonatomic, readonly) double longitude;
 
-- (id)initWithLatitude:(double)aLatitude longitude:(double)aLongitude sequence:(unsigned int)aSequence;
+- (id)initWithLatitude:(double)aLatitude longitude:(double)aLongitude;
 
 @end
 
 @interface Shape : NSObject {
 @private
     NSMutableArray *_points;
-    BOOL _needs_sort;
 }
 
-@property (nonatomic, retain) NSMutableArray *_points;
-@property BOOL _needs_sort;
+@property (nonatomic, strong) NSMutableArray *_points;
 
 - (void)addPoint:(ShapePoint *)point;
 - (NSArray *)points;
@@ -31,6 +25,6 @@
 @interface ShapeReducer : NSObject
 
 - (Shape*)reduce:(Shape*)aShape tolerance:(double)tolerance;
-- (void) douglasPeuckerReductionWithTolerance:(double)tolerance shape:(Shape*)shape outputShape:(Shape*)outputShape firstIndex:(int)first lastIndex:(int)last;
+- (void) douglasPeuckerReductionWithTolerance:(double)tolerance shape:(Shape*)shape outputShape:(Shape*)outputShape firstIndex:(NSUInteger)first lastIndex:(NSUInteger)last;
 + (double)orthogonalDistanceWithPoint:(ShapePoint *)point lineStart:(ShapePoint *)lineStart lineEnd:(ShapePoint *)lineEnd;
 @end
